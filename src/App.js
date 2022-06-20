65c1b6fc73be64c035fd11a0c253d728523ca546
supercoolx <dm.kondratov.git@gmail.com>
2022-06-20 04:02:32 -0700

    init project

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import CandyMachine from './CandyMachine';
+
+const App = () => {
+	const [walletAddress, setWalletAddress] = useState(null);
+
+	const checkIfWalletIsConnected = async () => {
+		try {
+			const { solana } = window;
+			if (solana?.isPhantom) {
+				console.log('Phantom wallet found!');
+				const response = await solana.connect({ onlyIfTrusted: true });
+				setWalletAddress(response.publicKey.toString()); //Guess Thats how you obtain address in SOL
+				console.log('Connected with Public Key:', response.publicKey.toString());
+			}
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	/*
+	 * Let's define this method so our code doesn't break.
+	 * We will write the logic for this next!
+	 */
+	const connectWallet = async () => {
+		const { solana } = window;
+
+		if (solana) {
+			const response = await solana.connect();
+			console.log("Connected with Public Key: ", response.publicKey.toString());
+			setWalletAddress(response.publicKey.toString())
+		}
+
+
+	};
+
+	/*
+	 * We want to render this UI when the user hasn't connected
+	 * their wallet to our app yet.
+	 */
+	const renderNotConnectedContainer = () => (
+		<button className="cta-button connect-wallet-button" onClick={connectWallet}>Connect to Wallet</button>
+	);
+
+	useEffect(() => {
+		checkIfWalletIsConnected().catch(err => console.error(err));
+	}, []);
+
+	return (
+		<div className="App">
+			<div className="container">
+				<div className="header-container">
+					<p className="header"> NFT DROP</p>
+					<p className="sub-text">Join The Meta Creatures Club</p>
+					{!walletAddress && renderNotConnectedContainer()}
+				</div>
+
+				{walletAddress && <CandyMachine walletAddress={window.solana} />}
+			</div>
+		</div>
+	);
+};
+
+export default App;
---
+++ src/CandyMachine/CountdownTime.js
@@ -0,0 +1,56 @@
+import { useEffect,useState } from "react";
+import './CountdownTimer.css';
+
+
+const CountdownTimer = ({dropDate}) => {
+
+    //State
+    const [timerString, setTimerString] = useState('');
+    useEffect(() => { 
+        console.log("Setting Inverval")
+    
+        //Use setInterval to run this piece of code every second
+         const interval = setInterval(() => {
+        const currentDate = new Date().getTime();
+        const distance = dropDate - currentDate;
+    
+        // Here it's as easy as doing some time math to get the different properties
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor(
+          (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        );
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+        // We have our desired output, set it in state!
+        setTimerString(`${days}d ${hours}h ${minutes}m ${seconds}s`);
+    
+        // If our distance passes zero this means that it's drop time!
+        if (distance < 0) {
+          console.log('Clearing interval...');
+          clearInterval(interval);
+        }
+    
+      },1000);
+      //Anytime our componet unmounts lets clearn up out interval
+      return() => { 
+        if(interval) { 
+          clearInterval(interval)
+        }
+      }
+    
+    
+    },[]);
+    
+    return ( 
+        <div className = "timer-container">
+            <p className = "timer-header">Meta Creatures Club Drop</p>
+            {timerString && <p className = "timer-value">{timerString}</p>}
+        </div>
+
+
+    )
+
+}
+
+export default CountdownTimer;
